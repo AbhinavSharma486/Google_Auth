0cdf1c0dfeac0ec4cbddd130313a9a248a08be07
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-02-08 23:36:48 +0530

    Frontend setup with tailwindcss , flowbite-react

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { Link } from "react-router-dom";
+import { Button } from "flowbite-react";
+
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Abhinav Sharma</h1>
+      <Button>Click me</Button>
+
+    </div>
+  );
+}
+
+export default App;
