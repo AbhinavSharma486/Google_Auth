c503093e6475ef175753c262bfce7e9dcfb0c977
AbhinavSharma486 <abhinavparashar486@gmail.com>
2025-02-10 15:33:04 +0530

    create signup page ui

---
+++ frontend/components/OAuth.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Button } from 'flowbite-react';
+import { AiFillGoogleCircle } from 'react-icons/ai';
+
+
+export default function OAuth() {
+
+  const handleGoogleClick = () => { };
+
+  return (
+    <Button type='button' gradientDuoTone='pinkToOrange' outline onClick={handleGoogleClick}>
+      <AiFillGoogleCircle className="w-6 h-6 mr-2" />
+      Continue with Google
+    </Button>
+  );
+}
+
